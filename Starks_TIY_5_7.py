cd78af816c9e08ec7d8499259f113ffed7045c0c
Julian <julian-jordan.starks@fwcsstudents.org>
2024-09-17 12:58:55 -0400

    Add files via upload

---
+++ Starks_TIY_5_7.py
@@ -0,0 +1,13 @@
+favorite_fruits = ['strawberries', 'oranges', 'grapes']
+
+
+if 'strawberries' in favorite_fruits:
+    print("You like eating strawberries, don't you?")
+if 'oranges' in favorite_fruits:
+    print("Oranges are good.")
+if 'grapes' in favorite_fruits:
+    print("Grapes are great in a fruit salad.")
+if 'watermelon' in favorite_fruits:
+    print("Watermelon is a great summer snack!")
+if 'bannana' in favorite_fruits:
+    print("Bannanas are great in smoothies.")
