ace704ac8b49c1b90c347036ca8d5f0a57e24dcb
Julian <julian-jordan.starks@fwcsstudents.org>
2024-09-17 12:56:15 -0400

    Add files via upload

---
+++ Starks_TIY_5_2.py
@@ -0,0 +1,35 @@
+sports = ['football', 'soccer', 'basketball']
+holidays = ['thanksgiving', 'leif errikson day', 'halloween']
+winter_holidays = 'christmas'
+number = 20
+
+
+print("Is 'football' in sports? I predict True.")
+print('football' in sports)
+
+print("\nIs 'hockey' in sports? I predict False.")
+print('hockey' in sports)
+
+print("\nIs 'christmas' not in holidays? I predict True.")
+print('christmas' not in holidays)
+
+print("\nIs 'thanksgiving' not in holidays? I predict False.")
+print('thanksgiving' not in holidays)
+
+print("\nIs 'number' == 20? I predict true.")
+print(number == 20)
+
+print("\nIs 'number' != 20? I predict false.")
+print(number != 20)
+
+print("\nIs 'number' < 21? I predict true.")
+print(number < 21)
+
+print("\nIs 'number' > 200? I predict false.")
+print(number > 200)
+
+print("\nIs 'football' and 'basketball' in sports? I predict True.")
+print('football' and 'basketball' in sports)
+
+print("\nIs 'football' and 'hockey' in sports? I predict false.")
+print('football' and 'hockey' in sports)
