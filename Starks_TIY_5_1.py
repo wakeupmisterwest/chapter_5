1dd268fa0fc8403b973ee25baac2b27d76d1d729
Julian <julian-jordan.starks@fwcsstudents.org>
2024-09-17 12:54:38 -0400

    Add files via upload

---
+++ Starks_TIY_5_1.py
@@ -0,0 +1,36 @@
+animal = 'dog'
+reptile = 'snake'
+season = 'Autumn'
+weather = 'sunny'
+color = 'purple'
+
+
+print("Is animal == 'dog'? I predict True.")
+print(animal == 'dog')
+
+print("\nIs dog == 'cat'? I predict False.")
+print(animal == 'cat')
+
+print("\nIs reptile == 'snake'? I predict True.")
+print(reptile == 'snake')
+
+print("\nIs reptile == 'lizard'? I predict False.")
+print(reptile == 'lizard')
+
+print("\nIs season == 'Autumn'? I predict True.")
+print(season == 'Autumn')
+
+print("\nIs season == 'Summer'? I predict False.")
+print(season == 'Summer')
+
+print("\nIs weather == sunny? I predict True.")
+print(weather == 'sunny')
+
+print("\nIs weather == cloudy? I predict False.")
+print(weather == 'cloudy')
+
+print("\nIs color == purple? I predict True.")
+print(color == 'purple')
+
+print("\nIs color == red? I predict False.")
+print(color == 'red')
