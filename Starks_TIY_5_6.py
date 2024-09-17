5c542253c9d10ec3d1d015fdc9f15001fe12de66
Julian <julian-jordan.starks@fwcsstudents.org>
2024-09-17 12:58:18 -0400

    Add files via upload

---
+++ Starks_TIY_5_6.py
@@ -0,0 +1,15 @@
+age = 4
+
+
+if age < 2:
+    print("You are a baby.")
+elif age < 4:
+    print("You are a toddler.")
+elif age <= 12:
+    print("You are a kid.")
+elif age <= 19:
+    print("You are a teenager.")
+elif age <= 64:
+    print("You are an adult.")
+else:
+    print("You are an elder.")
